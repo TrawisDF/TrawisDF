fe3f1667dc2c9578b03475b385fdf0c1be45f87a
TrawisDF <134593322+TrawisDF@users.noreply.github.com>
2024-02-08 16:58:24 +0700

    Finalize 2

--- src/components/FloatLink.jsx
+++ src/components/FloatLink.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 const FloatLink = () => {
   return (
     <>
@@ -14,12 +15,20 @@ const FloatLink = () => {
         </a>
       </div>
       <div className="floating-link" id="floating-link-3">
-        <a href="https://linkedin.com/in/tawid-khantiyoo-273146274" target="_blank">
+        <a
+          href="https://linkedin.com/in/tawid-khantiyoo-273146274"
+          target="_blank"
+        >
           <img src="linkedinicon.png" alt="LinkedIn Logo" />
         </a>
       </div>
+      <div className="floating-link" id="floating-link-4">
+        <a href="resume.pdf" target="_blank">
+          <img src="resumeIcon.png" alt="ResumeDownload" />
+        </a>
+      </div>
     </>
-  );
+  ); 
 };
 
 export default FloatLink;
